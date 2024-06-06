e14d96c2df080cc8ca6cf0e039909406d4290136
lucasclemente08 <you@example.com>
2024-06-06 11:00:26 -0300

    mejoras en empleados

--- src/pages/empleados/Empleados.jsx
+++ src/pages/empleados/Empleados.jsx
@@ -90,6 +90,9 @@ const Empleados=()=>{
     setModalAbierto(false);
   };
 
+  const cerrarModalMod = () => {
+    setModalAbiertoMod(false);
+  };
 
   const handleChange = (e) => {
     const { name, value } = e.target;
@@ -183,7 +186,7 @@ return(
       <button onClick={abrirModal} className="bg-green-600 hover:bg-green-700 text-white font-bold py-2 mt-2 px-4 rounded">
         Agregar empleado
       </button>
-      {modalAbiertoMod && (
+      {modalAbierto && (
                 <div className="fixed inset-0 overflow-y-auto">
                   <div className="flex items-center justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
                     <div className="fixed inset-0 transition-opacity" aria-hidden="true">
@@ -238,7 +241,7 @@ return(
                 </div>
               )}
 
-{modalAbierto &&  (
+{modalAbiertoMod &&  (
         <div className="fixed inset-0 overflow-y-auto">
           <div className="flex items-center justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
             <div className="fixed inset-0 transition-opacity" aria-hidden="true">
@@ -267,7 +270,7 @@ return(
                 <button onClick={handleSubmitModificar} type="button" className="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-green-600 text-base font-medium text-white hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-500 sm:ml-3 sm:w-auto sm:text-sm">
                   Modificar
                 </button>
-                <button onClick={cerrarModal} type="button" className="mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm">
+                <button onClick={cerrarModalMod} type="button" className="mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm">
                   Cancelar
                 </button>
               </div>
