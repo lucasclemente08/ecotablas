178a0af12799491f300140350eac13d66446ab6e
lucasclemente08 <you@example.com>
2024-08-26 11:29:39 -0300

    changes on material procesado and material triturado( add button for material)

--- src/pages/materialProc/materialProc.jsx
+++ src/pages/materialProc/materialProc.jsx
@@ -6,8 +6,49 @@ import { useEffect,useState } from 'react'
 const MaterialProc = () => {
 
   const [materials, setMaterials] = useState([]);
+  const[newMaterial, setNewMaterial] = useState(
+    {
+      VolumenP:'',
+      FechaIngresoP:'',
+      IdIngresoMaterial:''
+    }
+  );
+  const[modalAbierto,setModalAbierto]=useState(false);
+
+  const [mensaje, setMensaje] = useState("");
+
+  const abrirModal = () => {
+    setModalAbierto(true);
+  };
+  const cerrarModal = () => {
+    setModalAbierto(false)
+  }
 
 
+
+  const handleSubmit=()=>{
+    axios.post(`http://www.trazabilidadodsapi.somee.com/api/MaterialPros/Insertar`, newMaterial)
+    .then((response) => {
+      
+      setModalAbierto(false);
+      setMensaje("Inserción exitosa");
+      axios.get(`http://www.trazabilidadodsapi.somee.com/api/MaterialPros/ListarTodo`)
+        .then((response) => {
+      
+        })
+        .catch((error) => console.error('Error al obtener los datos:', error));
+    })
+    .catch((error) => console.error('Error al agregar el material:', error));
+  }
+  
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setNewMaterial(prevState => ({
+      ...prevState,
+      [name]: value
+    }));
+  };
+  
 
   
 useEffect(()=>{
@@ -28,7 +69,48 @@ useEffect(()=>{
       <div className="md:flex flex-row bg-slate-900 min-h-screen">
         <Home />
         <div className="p-4 w-full">
-          <h2 className="text-2xl font-bold text-white mb-4">Materiales Procesados</h2>
+          <h2 className="text-2xl font-bold text-white mb-4">Materiales Procesados</h2><button onClick={abrirModal} className="bg-green-600 hover:bg-green-700 text-white font-bold py-2 mt-2 mb-5 px-4 rounded">
+            Agregar material Procesado
+          </button>
+
+{modalAbierto && (
+            <div className="fixed inset-0 overflow-y-auto">
+            <div className="flex items-center justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
+              <div className="fixed inset-0 transition-opacity" aria-hidden="true">
+                <div className="absolute inset-0 bg-gray-500 opacity-75">
+           
+                </div>
+              </div>
+              
+              <span className="hidden sm:inline-block sm:align-middle sm:h-screen" aria-hidden="true">&#8203;</span>
+              <div className="inline-block align-bottom bg-white rounded-lg px-4 pt-5 pb-4 text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full sm:p-6">
+                <div>
+                  <div></div>
+                  <div className="mt-3 text-center sm:mt-5" >
+                    <h3 className="text-lg leading-6 font-medium text-gray-900">Agregar Materiales procesados</h3>
+                    <div className="mt-2">
+                      <input type="text" name="VolumenP" placeholder="Volumen *" value={newMaterial.VolumenP} onChange={handleChange} className="border p-2 w-full" />
+                      <input type="date" name="FechaIngresoP" placeholder="Fecha *" value={newMaterial.FechaIngresoP} onChange={handleChange} className="border p-2 w-full mt-2" />
+                      <input type="text" name="IdIngresoMaterial" placeholder="Material Clasificada *" value={newMaterial.IdIngresoMaterial} onChange={handleChange} className="border p-2 w-full mt-2" />
+                 
+                     
+                    </div>
+                  </div>
+                </div>
+                <div className="mt-5 sm:mt-6">
+                  <button onClick={handleSubmit} className="inline-flex justify-center w-full rounded-md border border-transparent shadow-sm px-4 py-2 bg-green-600 text-base font-medium text-white hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-500 sm:text-sm">
+                    Guardar
+                  </button>
+                  <button onClick={cerrarModal} className="mt-2 inline-flex justify-center w-full rounded-md border border-transparent shadow-sm px-4 py-2 bg-red-600 text-base font-medium text-white hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500 sm:text-sm">
+                    Cancelar
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+)
+
+}
           <div className="overflow-x-auto">
           <table className="min-w-full bg-white rounded-lg shadow-md">
               <thead>
@@ -42,7 +124,7 @@ useEffect(()=>{
               <tbody>
                 {materials.map((material) => (
                   <tr key={material.id} className="hover:bg-gray-100">
-                    <td className="border-b py-3 px-4">{material.IdIngresoMaterial}</td>
+                    <td className="border-b py-3 px-4">{material.IdMaterialProcesado}</td>
                     <td className="border-b py-3 px-4">Volumen: {material.VolumenP} kgs</td>
                     <td className="border-b py-3 px-4">{material.FechaIngresoP}</td>
                     {/* <td className="border flex justify-center py-3 px-4">
--- src/pages/materialTrit/materialTrit.jsx
+++ src/pages/materialTrit/materialTrit.jsx
@@ -4,6 +4,24 @@ import Home from '../home/Home';
 import axios from 'axios';
 const MaterialTrit = () => {
   const [materials, setMaterials] = useState([]);
+  const[newMaterial, setNewMaterial] = useState(
+    {
+      VolumenT:'',
+      Fecha:'',
+      IdMaterialTriturado:''
+    }
+  );
+  const[modalAbierto,setModalAbierto]=useState(false);
+
+  const [mensaje, setMensaje] = useState("");
+
+  const abrirModal = () => {
+    setModalAbierto(true);
+  };
+  const cerrarModal = () => {
+    setModalAbierto(false)
+  }
+
 
 useEffect(()=>{
   const fetchMaterials = async () => {
@@ -19,8 +37,28 @@ useEffect(()=>{
   fetchMaterials();
 },[])
 
+const handleSubmit=()=>{
+  axios.post(`http://www.trazabilidadodsapi.somee.com/api/MaterialTrit/Insertar`, newMaterial)
+  .then((response) => {
+    
+    setModalAbierto(false);
+    setMensaje("Inserción exitosa");
+    axios.get(`http://www.trazabilidadodsapi.somee.com/api/MaterialTrit/ListarTodo`)
+      .then((response) => {
+    
+      })
+      .catch((error) => console.error('Error al obtener los datos:', error));
+  })
+  .catch((error) => console.error('Error al agregar el material:', error));
+}
 
-
+const handleChange = (e) => {
+  const { name, value } = e.target;
+  setNewMaterial(prevState => ({
+    ...prevState,
+    [name]: value
+  }));
+};
 
 
 
@@ -30,6 +68,49 @@ useEffect(()=>{
         <Home />
         <div className="p-4 w-full">
           <h2 className="text-2xl font-bold text-white mb-4">Materiales Triturado</h2>
+          <button onClick={abrirModal} className="bg-green-600 hover:bg-green-700 text-white font-bold py-2 mt-2 mb-5 px-4 rounded">
+            Agregar material triturado
+          </button>
+
+{modalAbierto && (
+            <div className="fixed inset-0 overflow-y-auto">
+            <div className="flex items-center justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
+              <div className="fixed inset-0 transition-opacity" aria-hidden="true">
+                <div className="absolute inset-0 bg-gray-500 opacity-75">
+           
+                </div>
+              </div>
+              
+              <span className="hidden sm:inline-block sm:align-middle sm:h-screen" aria-hidden="true">&#8203;</span>
+              <div className="inline-block align-bottom bg-white rounded-lg px-4 pt-5 pb-4 text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full sm:p-6">
+                <div>
+                  <div></div>
+                  <div className="mt-3 text-center sm:mt-5" >
+                    <h3 className="text-lg leading-6 font-medium text-gray-900">Agregar Materiales triturado</h3>
+                    <div className="mt-2">
+                      <input type="text" name="VolumenT" placeholder="Volumen *" value={newMaterial.VolumenT} onChange={handleChange} className="border p-2 w-full" />
+                      <input type="date" name="fecha" placeholder="Fecha *" value={newMaterial.Fecha} onChange={handleChange} className="border p-2 w-full mt-2" />
+                      <input type="text" name="IdMaterialClasificado" placeholder="Material Clasificada *" value={newMaterial.IdMaterialTriturado} onChange={handleChange} className="border p-2 w-full mt-2" />
+                 
+                     
+                    </div>
+                  </div>
+                </div>
+                <div className="mt-5 sm:mt-6">
+                  <button onClick={handleSubmit} className="inline-flex justify-center w-full rounded-md border border-transparent shadow-sm px-4 py-2 bg-green-600 text-base font-medium text-white hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-500 sm:text-sm">
+                    Guardar
+                  </button>
+                  <button onClick={cerrarModal} className="mt-2 inline-flex justify-center w-full rounded-md border border-transparent shadow-sm px-4 py-2 bg-red-600 text-base font-medium text-white hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500 sm:text-sm">
+                    Cancelar
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+)
+
+}
+
           <div className="overflow-x-auto">
           <table className="min-w-full bg-white rounded-lg shadow-md">
               <thead>
