ee766f44c03e84debb9971357b23042f5e565458
lucasclemente08 <you@example.com>
2024-09-05 10:56:11 -0300

    finish agregar ubicacion modal

--- src/pages/recoUrbanos/recoUrbanos.jsx
+++ src/pages/recoUrbanos/recoUrbanos.jsx
@@ -90,12 +90,12 @@ const RecoUrbanos = () => {
 
           {modalAbierto && (
             <div className="fixed inset-0 overflow-y-auto">
-              <div className="flex items-center justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
+              <div className="flex items-center justify-center min-h-screen pt-6 px-4 pb-20 text-center sm:block">
                 <div className="fixed inset-0 transition-opacity" aria-hidden="true">
                   <div className="absolute inset-0 bg-gray-500 opacity-75"></div>
                 </div>
                 <span className="hidden sm:inline-block sm:align-middle sm:h-screen" aria-hidden="true">&#8203;</span>
-                <div className="inline-block align-bottom bg-white rounded-lg px-4 pt-5 pb-4 text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full sm:p-6">
+                <div className="inline-block align-bottom bg-white rounded-lg px-4 pt-5 pb-4 text-left overflow-hidden shadow-xl transform transition-all sm:align-middle  sm:p-6">
                   <div>
                     <h3 className="text-lg leading-6 font-medium text-gray-900">Agregar Ubicacion</h3>
                     <div className="mt-2">
@@ -104,7 +104,36 @@ const RecoUrbanos = () => {
                       <input type="text" name="Long" placeholder="Longitud *" value={newUbicacion.Long} onChange={handleChange} className="border p-2 w-full mt-2" />
                     </div>
                   </div>
-                  <div className="mt-5 sm:mt-6">
+                  <div className="p-2  flex justify-center">
+
+                 
+                  <MapContainer
+                
+                className="overflow-y-auto w-4 "
+                center={centerPosition}
+                zoom={12}
+                scrollWheelZoom={false}
+                whenReady={(map) => {
+                  map.target.on('click', function (e) {
+                    const { lat, lng } = e.latlng;
+                    setNewUbicacion({ Nombre: '', Lat: lat.toFixed(6), Long: lng.toFixed(6) });
+                    L.marker([lat, lng], { icon: DefaultIcon }).addTo(map.target);
+                  });
+                }}
+              >
+                <TileLayer
+                  attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                  url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                />
+                {locations.map((location, index) => (
+                  <Marker key={index} position={[location.Lat, location.Long]}>
+                    <Popup>{location.Nombre}</Popup>
+                  </Marker>
+                ))}
+              </MapContainer>
+              </div>
+
+                  <div className="mt-2 sm:mt-2">
                     <button onClick={handleSubmit} className="inline-flex justify-center w-full rounded-md border border-transparent shadow-sm px-4 py-2 bg-green-600 text-base font-medium text-white hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-500 sm:text-sm">
                       Guardar
                     </button>
@@ -132,15 +161,10 @@ const RecoUrbanos = () => {
                 id="map"
                 className="overflow-y-auto"
                 center={centerPosition}
-                zoom={10}
+                zoom={12}
                 scrollWheelZoom={false}
-                whenReady={(map) => {
-                  map.target.on('click', function (e) {
-                    const { lat, lng } = e.latlng;
-                    setNewUbicacion({ Nombre: '', Lat: lat.toFixed(6), Long: lng.toFixed(6) });
-                    L.marker([lat, lng], { icon: DefaultIcon }).addTo(map.target);
-                  });
-                }}
+                
+               
               >
                 <TileLayer
                   attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
