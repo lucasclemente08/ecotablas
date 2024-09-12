32db3eadf1384f7ae35ac68a9a2e0b5371e2ac0e
lucasclemente08 <you@example.com>
2024-09-12 11:23:31 -0300

    add button edit to materiaProc

--- src/pages/materialProc/materialProc.jsx
+++ src/pages/materialProc/materialProc.jsx
@@ -6,28 +6,35 @@ import PdfGenerator from '../../components/PdfGenerator';
 import TablaHead from '../../components/Thead';
 import DeleteButton from '../../components/DeleteButton';
 import AddModal from '../../components/AddModal';
+import ButtonEdit from '../../components/buttonEdit';
 
 const MaterialProc = () => {
   const [materials, setMaterials] = useState([]);
-  const [newMaterial, setNewMaterial] = useState({
-    VolumenP: '',
-    FechaIngresoP: '',
-    IdIngresoMaterial: ''
-  });
   const [modalAbierto, setModalAbierto] = useState(false);
   const [modalEdit, setModalEdit] = useState(false);
   const [mensaje, setMensaje] = useState("");
   const [materialId, setMaterialId] = useState(null);
-
+  
   const [formValues, setFormValues] = useState({
     VolumenP: '',
     FechaIngresoP: '',
     IdIngresoMaterial: '1'
   });
 
-
   const abrirModal = () => setModalAbierto(true);
   const cerrarModal = () => setModalAbierto(false);
+
+  const abrirModalEdit = (material) => {
+    setMaterialId(material.IdMaterialProcesado); // Guardar el ID del material seleccionado
+    setFormValues({
+      VolumenP: material.VolumenP,
+      FechaIngresoP: material.FechaIngresoP.slice(0, 10), // Formatear fecha si es necesario
+      IdIngresoMaterial: material.IdIngresoMaterial,
+    });
+    setModalEdit(true);
+  };
+  
+  const cerrarModalEdit = () => setModalEdit(false);
 
   const handleSubmit = () => {
     axios.post('http://www.trazabilidadodsapi.somee.com/api/MaterialPros/Insertar', formValues)
@@ -38,25 +45,20 @@ const MaterialProc = () => {
       .catch((error) => console.error('Error al agregar el material:', error));
   };
 
-
   const handleEditSubmit = () => {
-    if (!newMaterial.VolumenP || !newMaterial.FechaIngresoP || !newMaterial.IdIngresoMaterial) {
+    if (!formValues.VolumenP || !formValues.FechaIngresoP || !formValues.IdIngresoMaterial) {
       setMensaje("Todos los campos son obligatorios.");
       return;
     }
     
-
-
-
-
-    axios.put(`http://www.trazabilidadodsapi.somee.com/api/MaterialPros/Modificar/${materialId}`, newMaterial)
+    axios.put(`http://www.trazabilidadodsapi.somee.com/api/MaterialPros/Modificar/${materialId}`, formValues)
       .then(() => {
         setModalEdit(false);
         setMensaje("Modificación exitosa");
         fetchMaterials();
       })
       .catch((error) => console.error('Error al modificar el material:', error));
-  }
+  };
 
   const handleChange = (e) => {
     const { name, value } = e.target;
@@ -79,9 +81,7 @@ const MaterialProc = () => {
     fetchMaterials();
   }, []);
 
-  const title = ['Volumen (kgs) ', 'Fecha de ingreso','Acciones'];
-
-
+  const title = ['Volumen (kgs)', 'Fecha de ingreso', 'Acciones'];
 
   const columns = [
     { header: "Volumen (kgs)", dataKey: "VolumenP" },
@@ -90,7 +90,7 @@ const MaterialProc = () => {
 
   const rows = materials.map(material => ({
     VolumenP: `${material.VolumenP} kgs`,
-    FechaIngresoP: material.FechaIngresoP.slice(0, 10) // Asegúrate de que coincida con el dataKey
+    FechaIngresoP: material.FechaIngresoP.slice(0, 10)
   }));
 
   const fields = [
@@ -108,70 +108,55 @@ const MaterialProc = () => {
           <AddButton abrirModal={abrirModal} title={"Añadir Materiales procesados"} />
 
           <PdfGenerator columns={columns} data={materials} title="Reporte de Materiales procesados" />    
+          
           {modalAbierto && (
-             <AddModal
-             title="Agregar Material Procesado"
-             fields={fields}
-             handleChange={handleChange}
-             handleSubmit={handleSubmit}
-             cerrarModal={cerrarModal}
-             values={formValues}
-           />
+            <AddModal
+              title="Agregar Material Procesado"
+              fields={fields}
+              handleChange={handleChange}
+              handleSubmit={handleSubmit}
+              cerrarModal={cerrarModal}
+              values={formValues}
+            />
           )}
 
           {modalEdit && (
-            <div className="fixed inset-0 overflow-y-auto">
-              <div className="flex items-center justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
-                <div className="fixed inset-0 transition-opacity" aria-hidden="true">
-                  <div className="absolute inset-0 bg-gray-500 opacity-75"></div>
-                </div>
-                <span className="hidden sm:inline-block sm:align-middle sm:h-screen" aria-hidden="true">&#8203;</span>
-                <div className="inline-block align-bottom bg-white rounded-lg px-4 pt-5 pb-4 text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full sm:p-6">
-                  <div>
-                    <h3 className="text-lg leading-6 font-medium text-gray-900">Editar Material Procesado</h3>
-                    <div className="mt-2">
-                      <input type="text" name="VolumenP" placeholder="Volumen *" value={newMaterial.VolumenP} onChange={handleChange} className="border p-2 w-full" />
-                      <input type="date" name="FechaIngresoP" placeholder="Fecha *" value={newMaterial.FechaIngresoP} onChange={handleChange} className="border p-2 w-full mt-2" />
-                      <input type="text" name="IdIngresoMaterial" placeholder="Material Clasificada *" value={newMaterial.IdIngresoMaterial} onChange={handleChange} className="border p-2 w-full mt-2" />
-                    </div>
-                  </div>
-                  <div className="mt-5 sm:mt-6">
-                    <button onClick={handleEditSubmit} className="inline-flex justify-center w-full rounded-md border border-transparent shadow-sm px-4 py-2 bg-blue-600 text-base font-medium text-white hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 sm:text-sm">
-                      Guardar Cambios
-                    </button>
-                    <button onClick={cerrarModalEdit} className="mt-2 inline-flex justify-center w-full rounded-md border border-transparent shadow-sm px-4 py-2 bg-red-600 text-base font-medium text-white hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500 sm:text-sm">
-                      Cancelar
-                    </button>
-                  </div>
-                </div>
-              </div>
-            </div>
+            <ButtonEdit
+              title="Material"
+              fields={fields}
+              id={materialId}
+              formValues={formValues}
+              handleChange={handleChange}
+              handleEditSubmit={handleEditSubmit}
+              cerrarModalEdit={cerrarModalEdit}
+              abrirModal={abrirModalEdit}
+            />
           )}
 
           <div className="overflow-x-auto">
             <table className="min-w-full bg-white rounded-lg shadow-md">
-  <TablaHead titles={title} />
-
+              <TablaHead titles={title} />
               <tbody>
                 {materials.map((material) => (
                   <tr key={material.IdMaterialProcesado} className="hover:bg-gray-100">
-                 
                     <td className="border-b py-3 px-4">Volumen: {material.VolumenP} kgs</td>
                     <td className="border-b py-3 px-4">{material.FechaIngresoP.slice(0, 10)}</td>
                     <td className="border-b py-3 px-4 flex justify-center">
-                    <button 
-  onClick={() => abrirModalEdit(material)}
-  className="bg-yellow-600 hover:bg-yellow-700 text-white font-bold py-2 px-4 rounded transition duration-300 ease-in-out transform hover:scale-105"
->
-  Modificar
-</button>
-
-<DeleteButton     
-id={material.IdMaterialProcesado}
-                  endpoint="http://www.trazabilidadodsapi.somee.com/api/MaterialPros/Borrar" // Ajusta el endpoint según sea necesario
-                 updateList={fetchMaterials} // Pasa la función para actualizar la lista
-                />
-
+                      <ButtonEdit
+                        title="Material"
+                        fields={fields}
+                        id={material.IdMaterialProcesado}
+                        formValues={formValues}
+                        handleChange={handleChange}
+                        handleEditSubmit={handleEditSubmit}
+                        cerrarModalEdit={cerrarModalEdit}
+                        abrirModal={abrirModalEdit} // Pasa la función para abrir el modal
+                      />
+                      <DeleteButton     
+                        id={material.IdMaterialProcesado}
+                        endpoint="http://www.trazabilidadodsapi.somee.com/api/MaterialPros/Borrar"
+                        updateList={fetchMaterials}
+                      />
                     </td>
                   </tr>
                 ))}
