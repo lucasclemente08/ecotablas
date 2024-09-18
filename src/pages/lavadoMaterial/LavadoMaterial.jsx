81fd468569bb378a7dae91b51ecb496263fe839a
Lucas Clemente <lucasclemente2016@gmail.com>
2024-09-18 20:25:19 -0300

    add SectionLayout for reduce and improve code

--- src/pages/lavadoMaterial/LavadoMaterial.jsx
+++ src/pages/lavadoMaterial/LavadoMaterial.jsx
@@ -1,23 +1,14 @@
-import React from 'react'
-import Home from '../home/Home'
-import ReportButton from '../../components/ReportButton'
+import React from 'react';
+import Home from '../home/Home';
+import SectionLayout from '../../layout/SectionLayout'; // Ajusta la ruta según sea necesario
+import ReportButton from '../../components/ReportButton';
+
 const LavadoMaterial = () => {
   return (
-    <>
-  <section className="md:flex flex-row bg-slate-900 min-h-screen">
-        <Home />
-        <div className="p-4 w-full">
-          <h2 className="text-2xl font-bold text-white mb-4">
-            Lavado de materiales
-          </h2>
+    <SectionLayout title="Lavado de materiales">
+      <ReportButton />
+    </SectionLayout>
+  );
+};
 
-        <ReportButton />
-
-        </div>
-      </section>
-
-    </>
-  )
-}
-
-export default LavadoMaterial
+export default LavadoMaterial;
