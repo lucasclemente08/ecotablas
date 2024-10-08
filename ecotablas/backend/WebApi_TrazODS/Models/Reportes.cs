be3a2ee11ed0a875ae9d8ea7a220834262e6ada7
lucasclemente08 <you@example.com>
2024-10-08 10:53:50 -0300

    add Reportes

---
+++ ecotablas/backend/WebApi_TrazODS/Models/Reportes.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+
+namespace WebApi_TrazODS.Models
+{
+    public class Reportes
+    {
+        #region Atributos
+        private readonly string connectionString;
+
+        public Reportes(IConfiguration configuration)
+        {
+            connectionString = configuration.GetConnectionString("DefaultConnection");
+        }
+        #endregion
+
+        #region Propiedades
+        public int Id_reporte { get; set; }
+        public string Titulo { get; set; }
+        public string Descripcion { get; set; }
+        public string Area { get; set; }
+        public DateTime FechaIncidente { get; set; }
+        public DateTime FechaReporte { get; set; } = DateTime.Now;
+        public string Estado { get; set; } = "Pendiente";
+        #endregion
+
+        #region Metodos
+        public DataTable SelectAll()
+        {
+            string sqlSentencia = "SP_ObtenerReportes";
+            DataTable dataTable = new DataTable();
+
+            using (SqlConnection sqlCnn = new SqlConnection(connectionString))
+            {
+                try
+                {
+                    sqlCnn.Open();
+                    using (SqlCommand sqlCom = new SqlCommand(sqlSentencia, sqlCnn))
+                    {
+                        sqlCom.CommandType = CommandType.StoredProcedure;
+
+                        using (SqlDataAdapter da = new SqlDataAdapter(sqlCom))
+                        {
+                            da.Fill(dataTable);
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+         
+                    Console.WriteLine(ex.Message); 
+                }
+            }
+
+            return dataTable; 
+        }
+        #endregion
+    }
+}
