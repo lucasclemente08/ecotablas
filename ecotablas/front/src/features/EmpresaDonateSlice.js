20b8457317454cba8b96b1c6f4ea1cc8d009f443
Lucas Clemente <lucasclemente2016@gmail.com>
2024-10-16 20:52:38 -0300

    add empresasDonante

--- ecotablas/front/src/features/EmpresaDonateSlice.js
+++ ecotablas/front/src/features/EmpresaDonateSlice.js
@@ -3,30 +3,14 @@ import axios from 'axios';
 import builderApiUrl from '../utils/BuilderApi';
 
 
-export const fetchTolva = createAsyncThunk('tolva/fetchTolva', async () => {
+export const fetchEmpresa = createAsyncThunk('empresa/', async () => {
     const response = await axios.get(builderApiUrl('EmpresaD'));
     return response.data;
   });
+
   
-  export const addTolva = createAsyncThunk('tolva/addTolva', async (formValues) => {
-    const response = await axios.post(builderApiUrl('Tolva/Agregar'), formValues);
-    return response.data;
-  });
-  
-  
-  export const editTolva = createAsyncThunk('tolva/editTolva', async ({ id, formValues }) => {
-    const response = await axios.put(builderApiUrl(`Tolva/Modificar/${id}`), formValues);
-    return response.data;
-  });
-  
-  
-  export const deleteTolva = createAsyncThunk('tolva/deleteTolva', async (id) => {
-    await axios.delete(builderApiUrl(`Tolva/Borrar/${id}`));
-    return id;
-  });
-  
-  const tolvaSlice = createSlice({
-    name: 'tolva',
+  const EmpresaSlice = createSlice({
+    name: 'Empresa',
     initialState: {
       data: [],
       loading: false,
@@ -47,21 +31,9 @@ export const fetchTolva = createAsyncThunk('tolva/fetchTolva', async () => {
           state.loading = false;
           state.error = action.error.message;
         })
-        // Add new item
-        .addCase(addTolva.fulfilled, (state, action) => {
-          state.data.push(action.payload);
-        })
-        // Edit item
-        .addCase(editTolva.fulfilled, (state, action) => {
-          const index = state.data.findIndex((item) => item.ID === action.payload.ID);
-          state.data[index] = action.payload;
-        })
-        // Delete item
-        .addCase(deleteTolva.fulfilled, (state, action) => {
-          state.data = state.data.filter((item) => item.ID !== action.payload);
-        });
+
     },
   });
   
-  export default tolvaSlice.reducer;
+  export default EmpresaSlice.reducer;
   
