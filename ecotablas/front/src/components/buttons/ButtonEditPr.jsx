1c7d1bcc4bec4185e9788e6453134ae4628db4f8
Lucas Clemente <lucasclemente2016@gmail.com>
2024-11-08 20:25:34 -0300

    prueba deploy

---
+++ ecotablas/front/src/components/buttons/ButtonEditPr.jsx
@@ -0,0 +1,87 @@
+import React from "react";
+
+const ButtonEdit = ({
+  title,
+  fields,
+  id,
+  formValues,
+  handleChange,
+  handleEditSubmit,
+  cerrarModalEdit,
+}) => {
+  return (
+    <div className="fixed inset-0 overflow-y-auto">
+      <div className="flex items-center justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
+        <div className="fixed inset-0 transition-opacity" aria-hidden="true">
+          <div className="absolute inset-0 bg-gray-500 opacity-75"></div>
+        </div>
+        <span
+          className="hidden sm:inline-block sm:align-middle sm:h-screen"
+          aria-hidden="true"
+        >
+          &#8203;
+        </span>
+        <div className="inline-block align-bottom bg-white rounded-lg px-4 pt-5 pb-4 text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full sm:p-6">
+          <div>
+            <h3 className="text-lg leading-6 font-medium text-gray-900">
+              Editar {title}
+            </h3>
+            <div className="mt-2">
+              <form onSubmit={handleEditSubmit}>
+                {fields.map((field) => (
+                  <div key={field.name} className="mt-2">
+                    {field.type === "select" ? (
+                      <select
+                        name={field.name}
+                        value={formValues[field.name] || ""}
+                        onChange={handleChange}
+                        className="border p-2 w-full"
+                      >
+                        <option value="">Selecciona una opción</option>
+                        {field.options.map((option) => (
+                          <option key={option.value} value={option.value}>
+                            {option.label}
+                          </option>
+                        ))}
+                      </select>
+                    ) : (
+                      <input
+                        type={field.type}
+                        name={field.name}
+                        placeholder={field.placeholder}
+                        value={formValues[field.name] || ""}
+                        onChange={handleChange}
+                        className="border p-2 w-full"
+                      />
+                    )}
+                  </div>
+                ))}
+                <div className="mt-5 sm:mt-6">
+                  <button
+
+                  onClick={handleEditSubmit}
+             type="button"
+                    className="inline-flex justify-center w-full rounded-md border border-transparent shadow-sm px-4 py-2 bg-blue-600 text-base font-medium text-white hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 sm:text-sm"
+                  >
+                    Guardar
+                  </button>
+                </div>
+              </form>
+              <div className="mt-2">
+                <button
+                  type="button"
+                  onClick={cerrarModalEdit}
+                  className="inline-flex justify-center w-full rounded-md border border-transparent shadow-sm px-4 py-2 bg-red-600 text-base font-medium text-white hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500 sm:text-sm"
+                >
+                  Cancelar
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ButtonEdit;
--- ecotablas/front/src/pages/materialTrit/materialTrit.jsx
+++ ecotablas/front/src/pages/materialTrit/materialTrit.jsx
@@ -5,7 +5,7 @@ import PdfGenerator from "../../components/buttons/PdfGenerator";
 import TablaHead from "../../components/Thead";
 import DeleteButton from "../../components/buttons/DeleteButton";
 import AddModal from "../../components/AddModal";
-import ButtonEdit from "../../components/buttons/ButtonEdit";
+import ButtonEdit from "../../components/buttons/ButtonEditPr";
 import LoadingTable from "../../components/LoadingTable";
 import NextButton from "../../components/buttons/NextButton";
 import NextModal from "../../components/NextModal";
