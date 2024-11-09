bb9bf3a9681b6dfb81b25b420754a7a2b656a8b7
Lucas Clemente <lucasclemente2016@gmail.com>
2024-11-09 12:37:00 -0300

    delete back

--- ecotablas/backend/WebApi_TrazODS/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace WebApi_TrazODS.Areas.HelpPage.ModelDescriptions
-{
-    public class DictionaryModelDescription : KeyValuePairModelDescription
-    {
-    }
-}
--- ecotablas/backend/WebApi_TrazODS/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace WebApi_TrazODS.Areas.HelpPage.ModelDescriptions
-{
-    public class SimpleTypeModelDescription : ModelDescription
-    {
-    }
-}
