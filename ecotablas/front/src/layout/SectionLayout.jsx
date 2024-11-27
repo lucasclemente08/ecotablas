93d7838c0a4d230cd8c28a53b09ba5b0e1b5229a
lucasclemente08 <lucasclemente2016@gmail.com>
2024-11-27 11:45:25 -0300

    fix sidebar

--- ecotablas/front/src/layout/SectionLayout.jsx
+++ ecotablas/front/src/layout/SectionLayout.jsx
@@ -1,23 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 import Sidebar from "../layout/sidebar";
 import Navbar from "../layout/Navbar";
-const SectionLayout = ({ title, children }) => {
-  return (
-    <div className="bg-slate-900  min-h-screen flex">
-      {/* Sidebar fijo */}
 
+const SectionLayout = ({ title, children }) => {
+  const [isSidebarOpen, setSidebarOpen] = useState(true);
+
+  return (
+    <div className="bg-slate-900 min-h-screen flex">
+      {/* Navbar móvil */}
       <div className="block md:hidden">
-          <Navbar />
-        </div>
-      <div className="hidden md:block fixed top-0 left-0 h-full w-[250px]">
-        <Sidebar />
+        <Navbar />
+      </div>
+
+      {/* Sidebar fijo */}
+      <div
+        className={`hidden md:block fixed top-0 left-0 h-full ${
+          isSidebarOpen ? "w-[250px]" : "w-[80px]"
+        } transition-all duration-300`}
+      >
+        <Sidebar isOpen={isSidebarOpen} toggleSidebar={() => setSidebarOpen(!isSidebarOpen)} />
       </div>
 
       {/* Contenido principal */}
-      <div className="flex-1 ml-0 md:ml-[250px] p-4">
-        <h2 className="text-2xl font-bold text-white mb-4">{title}</h2>
-        {children}
-      </div>
+      <div
+  className="flex-1 p-4 transition-all duration-300"
+  style={{
+    marginLeft: isSidebarOpen ? "250px" : "80px",  // Aplica el margen dinámicamente
+  }}
+>
+  <h2 className="text-2xl font-bold text-white mb-4">{title}</h2>
+  {children}
+</div>
+
     </div>
   );
 };
