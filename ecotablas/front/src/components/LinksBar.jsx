20789602709faa42a07bef0b4b8c483e91bd90da
lucasclemente08 <lucasclemente2016@gmail.com>
2025-02-13 08:39:26 -0300

    add fix links

--- ecotablas/front/src/components/LinksBar.jsx
+++ ecotablas/front/src/components/LinksBar.jsx
@@ -2,9 +2,19 @@ import React from "react";
 import MenuSection from "./MenuSection";
 import { ImTruck } from "react-icons/im";
 import { RiTeamFill, RiRecycleFill } from "react-icons/ri";
-import { FaTools, FaDollarSign } from "react-icons/fa";
+import { FaTools, FaDollarSign,FaUserPlus } from "react-icons/fa";
+import { useEffect } from "react";
 
+import { useRole } from "../context/RoleContext";
 const LinksBar = ({ isOpen }) => {
+
+  const role=useRole();
+  
+
+useEffect(() => {
+  console.log(role);
+},[])
+
   const employeeMenus = [
     { title: "Empleados", link: "/empleados" },
     { title: "Áreas y turnos de trabajo", link: "/areas" },
@@ -36,14 +46,22 @@ const LinksBar = ({ isOpen }) => {
     { title: "Gastos de vehículos", link: "/gastos/vehiculos" },
     { title: "Gastos de maquinaria", link: "/gastos/maquinaria" },
   ];
+  
 
+  const adminMenus = [
+    { title: "Gestión de Usuarios", link: "/admin" },
+    // { title: "Configuración del Sistema", link: "/admin/config" },
+    // { title: "Reportes", link: "/admin/reportes" },
+  ];
   return (
     <>
+ {role.role === "admin" && <MenuSection title="Administración" menus={adminMenus} icon={<FaUserPlus />} isOpen={isOpen} />}
       <MenuSection title="Empleados" menus={employeeMenus} icon={<RiTeamFill />} isOpen={isOpen} />
       <MenuSection title="Recolección" menus={urbanMenus} icon={<ImTruck />} isOpen={isOpen} />
       <MenuSection title="Materiales" menus={materialMenus} icon={<RiRecycleFill />} isOpen={isOpen} />
       <MenuSection title="Maquinaria" menus={machinesMenus} icon={<FaTools />} isOpen={isOpen} />
       <MenuSection title="Gastos" menus={ExpensesMenus} icon={<FaDollarSign />} isOpen={isOpen} />
+
     </>
   );
 };
